dcf7cf30f03f84d62a90a50d873c2881cb09cf85
fittersoutfit-blip <fittersoutfit@gmail.com>
2026-05-16 18:04:12 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+// BrightDays App Entry Point
+// This file imports the main BrightDays component.
+// The full app logic lives in BrightDays_5.jsx (copy it into this folder as BrightDays.jsx)
+
+import BrightDays from './BrightDays.jsx'
+
+export default function App() {
+  return <BrightDays />
+}
